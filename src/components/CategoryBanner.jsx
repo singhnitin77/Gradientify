ba7d554b909a78a490b3b2db640238b426ff20db
Nitin Singh <nitinsinghofficial9@gmail.com>
2022-08-11 23:52:24 +0530

    added teaser link

--- src/components/CategoryBanner.jsx
+++ src/components/CategoryBanner.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SingleCategory } from ".";
 import { Link } from "react-router-dom";
 
 const CategoryBanner = () => {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { YtVideo } from "./";
-import svgicon from "../assets/icon.svg";
 import banner from "../assets/banner.png";
 import { BsFillPlayFill, BsLightning } from "react-icons/bs";
 import Parallax from "parallax-js";
 import { NavHeader } from "./";
 import { AllGradients } from "../containers";
-import AnimatedLetters from "./AnimatedLetters/AnimatedLetters";
 import { IoCloseOutline } from "react-icons/io5";
 import { BiLoaderAlt } from "react-icons/bi";
 
@@ -69,9 +67,6 @@ const Hero = () => {
                   data-depth="0.3"
                   className="h-[195px] w-[344px] lg:h-[340px] lg:w-[604px] rounded-md border border-[#B84F90]"
                 />
-                {/* <div className="bgimage z-999 bg-cover bg-center p-4 absolute left-0 right-0 mx-auto top-[30%] transform duration-500 hover:scale-125 cursor-pointer border border-[#f5ba32]">
-                  <BsFillPlayFill className="text-white text-3xl" />
-                </div> */}
               </div>
 
               <button onClick={openModal} className="">
@@ -102,7 +97,7 @@ const Hero = () => {
                             loading="lazy"
                             width="956"
                             height="538"
-                            src="https://www.youtube.com/embed/c9l9x53v13o"
+                            src="https://www.youtube.com/embed/R0v9AS3gARc?autoplay=1"
                             title="Gradientify"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
@@ -125,126 +120,3 @@ const Hero = () => {
 };
 
 export default Hero;
-
-/* import React, { useEffect, useState } from "react";
-
-
-import axios from "axios"; // axios
-
-const Hero = ({ setOpen, user, setUser }) => {
-  const [starCount, setStarCount] = useState(1);
-  const [showYt, setShowYt] = useState(false);
-
-
-  return (
-    <div className="min-h-screen bg-image w-full text-[#ECF2F5] overflow-visible z-10">
-      <Header setOpen={setOpen} user={user} setUser={setUser} />
-      <YTModal open={showYt} setOpen={setShowYt} />
-      <div className="flex items-center justify-center h-auto min-h-[90vh] lg:h-[90vh] xl:h-[90vh] w-full flex-col lg:flex-row xl:flex-row overflow-hidden">
-        
-        <div className="w-full lg:w-6/12 xl:w-6/12 h-auto lg:h-full xl:h-full flex items-center justify-center flex-col relative bg-pattern-hero pb-10 lg:pb-0 xl:pb-0 animate__animated animate__fadeInRight">
-          // <div className="absolute h-full w-full flex items-center justify-center">
-          //   // <img
-          //   //   src="/assets/icon.svg"
-          //   //   className="w-[320px] lg:w-[450px] xl:w-[450px] opacity-50 lg:opacity-100 xl:opacity-100"
-          //   // />
-          // </div>
-          <div className="relative flex items-center justify-center">
-            <div className="flex">
-              <div id="scene">
-                <img
-                  data-depth="0.3"
-                  src="/assets/codehouse-banner.png"
-                  className="h-[189px] lg:h-[315px] w-[336px] lg:w-[560px] rounded-md border border-[#B84F90]"
-                />
-              </div>
-              <div
-                className="bg-image rounded-full p-4 absolute absolute-center transform duration-500 hover:scale-125 cursor-pointer border border-[#F5BA32]"
-                onClick={() => setShowYt(true)}
-              >
-                <BsFillPlayFill className="text-white text-3xl" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    
-  );
-};
-
-export default Hero; */
-
-/* 
-
-const Home = () => {
-  const [letterClass, setLetterClass] = useState("text-animate");
-
-  const nameArray = ["l", "o", "b", "o", "d", "a", "n"];
-  const jobArray = [
-    "w",
-    "e",
-    "b",
-    " ",
-    "d",
-    "e",
-    "v",
-    "e",
-    "l",
-    "o",
-    "p",
-    "e",
-    "r",
-    ".",
-  ];
-
-  useEffect(() => {
-    return setTimeout(() => {
-      setLetterClass("text-animate-hover");
-    }, 4000);
-  }, []);
-
-  return (
-    <>
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
-            <br />
-            <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m</span>
-            <img
-              src={LogoTitle}
-              alt="JavaScript Developer Name, Web Developer Name"
-            />
-            <AnimatedLetters
-              letterclassName={letterClass}
-              strArray={nameArray}
-              idx={15}
-            />
-            <br />
-            <AnimatedLetters
-              letterclassName={letterClass}
-              strArray={jobArray}
-              idx={22}
-            />
-          </h1>
-
-
-          <h2>Front End Developer / JavaScript Expert / Youtuber</h2>
-          <Link to="/contact" className="flat-button">
-            CONTACT ME
-          </Link>
-        </div>
-        <Logo />
-      </div>
-
-      <Loader type="pacman" />
-    </>
-  );
-};
-
-export default Home;
-
-*/
